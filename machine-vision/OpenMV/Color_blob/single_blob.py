d35fc571098c949e449a12504ffb5d6ee14839a6
zengwangfa <508293391@qq.com>
2019-06-20 20:43:06 +0800

    【bugs】修复了未识别到未返回数据包的问题

--- machine-vision/OpenMV/Color_blob/single_blob.py
+++ machine-vision/OpenMV/Color_blob/single_blob.py
@@ -5,6 +5,7 @@ LEDB = pyb.LED(1) # Red LED = 2, Green LED = 3, Blue LED = 1, IR LEDs = 4.
 LEDR = pyb.LED(2)
 LEDG = pyb.LED(3)
 
+flag = 0
 def InitSuccess_LED():
     for i in range(2):
         LEDG.on()
@@ -40,19 +41,42 @@ def get_length(blob):
     return int(length)
 
 # 色块检测数据打包
-def pack_blob_data(blob,ctrl):
+def pack_blob_data(blob,ctrl,flag):
 
-    #---包头------功能位----数据个数位----------------------------------数据包------------------------------------------累加和校验
-    #【AA 55】    【01】     【09】      【阈值、色块个数、识别度、简单距离、色块中心坐标(高8位x，低8位x,高8位y，低8位y)、res】     【sum】
+    #---包头------功能位----数据个数位---------------------------------数据包------------------------------------------------累加和校验
+    #【AA 55】    【01】     【0x0A】      【阈值、色块个数、识别度、简单距离、色块中心坐标(高8位x，低8位x,高8位y，低8位y)、res】     【sum】
 
-    datalist = [0xAA,0x55,ctrl.WorkMode,0x09,
+    datalist = [0xAA,0x55,ctrl.WorkMode,0x0A,
     ctrl.Threshold_index,
     blob.count(),
     int(blob.density()*100),
     get_length(blob),
     blob.cx()>>8,blob.cx(),
     blob.cy()>>8,blob.cy(),
-    0x00] # 定义返回数据列表
+    0x00,flag] # 定义返回数据列表
+
+    print(datalist)
+    datalist.append(sum_checkout(datalist))# 在list尾插入累加和校验
+    data = bytearray(datalist)
+    for res in data:
+        print("%x" %res)
+    #print(data)
+    return data
+
+# 色块未检测数据打包
+def pack_no_blob_data(ctrl,flag):
+
+    #---包头------功能位----数据个数位---------------------------------数据包------------------------------------------------累加和校验
+    #【AA 55】    【01】     【0x0A】      【阈值、色块个数、识别度、简单距离、色块中心坐标(高8位x，低8位x,高8位y，低8位y)、res】     【sum】
+
+    datalist = [0xAA,0x55,ctrl.WorkMode,0x0A,
+    ctrl.Threshold_index,
+    0x00,
+    0x00,
+    0x00,
+    0x00,0x00,
+    0x00,0x00,
+    0x00,flag] # 定义返回数据列表
 
     print(datalist)
     datalist.append(sum_checkout(datalist))# 在list尾插入累加和校验
@@ -78,19 +102,22 @@ def single_blob_LEDStatus(blobs,ctrl):
 
 
 
-def check_blob(img,ctrl,thresholds,threshold_index,uart):
+def single_blob(img,ctrl,thresholds,threshold_index,uart):
     blobs = img.find_blobs([thresholds[threshold_index]], pixels_threshold=5, area_threshold=5, merge=True)
     # pixels_threshold为像素阈值，只有大于pixels_threshold才会被识别出来，area_threshold 为面积阈值，只有大于area_threshold才能被识别出来
     # merge=True 合并图像中所有重叠的斑点
     if blobs:
+        flag = 0x01
         max_blob = find_max(blobs) #找到最大的色块
         img.draw_rectangle(max_blob.rect()) #画矩形
         img.draw_cross(max_blob.cx(), max_blob.cy())#中心画十字
         #---包头------功能位----数据个数位----------------------------------数据包------------------------------------------累加和校验
         #【AA 55】    【01】     【09】      【阈值、色块个数、识别度、简单距离、色块中心坐标(高8位x，低8位x,高8位y，低8位y)、res】   【sum】
-        uart.write(pack_blob_data(max_blob,ctrl))
-        LEDR.on()
+        uart.write(pack_blob_data(max_blob,ctrl,flag))
+        LEDG.on()
     else:
-        LEDR.off()
+        flag = 0x00
+        LEDG.off()
+        uart.write(pack_no_blob_data(ctrl,flag))
 
     single_blob_LEDStatus(blobs,ctrl)#LED状态
